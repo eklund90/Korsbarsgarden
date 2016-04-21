6a6ce089ec49a9b34444a8c2ecf467689c827009
jisa1400 <jisa1400@student.miun.se>
2016-04-21 11:18:55 +0200

    Revert "fyller textrutor"
    This reverts commit 759ef615b165409fd1b39c161f016d4bcabb762f.

--- Korsbarsgarden/skapakonto.aspx.cs
+++ Korsbarsgarden/skapakonto.aspx.cs
@@ -18,24 +18,14 @@ namespace Korsbarsgarden
         List<medlem> memberList = new List<medlem>();
         protected void Page_Load(object sender, EventArgs e)
         {
-        if(!IsPostBack)
-        {
             PanelResponse_skapakonto.Visible = false;        
             memberList = getMemberList();
-            foreach (medlem m in memberList)
+            foreach(medlem m in memberList)
             {
-                ListItem Data = new ListItem();
-                Data.Text = m.fnamn;
-                Data.Value = m.id.ToString();
-                medlemlist.Items.Add(Data);
-
-                //medlemlist.Items.Add(m.fnamn.ToString() + " " + m.enamn.ToString());
-                //medlemlist.DataSource = getMemberList();
-                //medlemlist.DataBind();
-            }           
+                medlemlist.Items.Add(m.fnamn.ToString() + " " + m.enamn.ToString());
+            }
+            
         }
-        }
-     
         protected void btn_skapakonto_Click(object sender, EventArgs e)
         {
             Encryption SHA256 = new Encryption();
@@ -58,18 +48,25 @@ namespace Korsbarsgarden
         }
 
         #region metoder
-        public void fyllmedlem(medlem nyMember)
-        {  
-                txtBox_skapakonto_fornamn.Text = nyMember.fnamn;
-                txtBox_skapakonto_efternamn.Text = nyMember.enamn;
-                txtBox_skapakonto_personnr.Text = nyMember.personnr;
-                txtBox_skapakonto_telefonnr.Text = nyMember.telefonnr;
-                txtBox_skapakonto_adress.Text = nyMember.adress;
-                txtBox_skapakonto_postnr.Text = nyMember.postnr;
-                txtBox_skapakonto_postort.Text = nyMember.postort;
-                txtBox_skapakonto_epost.Text = nyMember.epost;
-                dropdown_skapakonto_behorighet.Text = Convert.ToInt16(nyMember.behorighet).ToString();
-            
+        public void fyllmedlem(int id_member)
+        {
+            //medlem nyMember = new medlem();
+            //nyMember = methods.getMember(id_member);
+            //if (nyMember.memberId > 0)
+            //{
+
+            //    txtBox_skapakonto_fornamn.Text = nyMember.firstName;
+            //    txtBox_skapakonto_efternamn.Text = newMember.lastName;
+            //    txtBox_skapakonto_personnr.Text = newMember.address;
+            //    txtBox_skapakonto_telefonnr.Text = newMember.postalCode;
+            //    txtBox_skapakonto_adress.Text = newMember.city;
+            //    txtBox_skapakonto_postnr.Text = newMember.mail;
+            //    txtBox_skapakonto_postort.Text = newMember.gender;
+            //    txtBox_skapakonto_epost.Text =
+            //    dropdown_skapakonto_behorighet.Text = newMember.hcp.ToString();
+            //    tbMail.Text = newMember.mail;
+            //    tbGolfId.Text = newMember.golfId;
+            //}
         }
         public void läggtillmedlem(medlem nymedlem)
         {
@@ -119,6 +116,7 @@ namespace Korsbarsgarden
             List<medlem> memberList = new List<medlem>();
             NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["korsbarsgarden"].ConnectionString);
             string sql;
+
             try
             {
                 conn.Open();
@@ -127,19 +125,17 @@ namespace Korsbarsgarden
                 NpgsqlDataReader dr = command.ExecuteReader();
                 while (dr.Read())
                 {
-                    memberList.Add(new medlem {id = (int)dr["id"], fnamn = dr["fnamn"].ToString() + " " + dr["enamn"].ToString()});
-                    //medlem nymedlem = new medlem();
-                    //nymedlem.fnamn = (string)(dr["fnamn"]);
-                    //nymedlem.enamn = (string)(dr["enamn"]);
-                    //nymedlem.personnr = (string)(dr["personnr"]);
-                    //nymedlem.telefonnr = (string)(dr["telefonnr"]);
-                    //nymedlem.adress = (string)(dr["adress"]);
-                    //nymedlem.postnr = (string)(dr["postnr"]);
-                    //nymedlem.postort = (string)(dr["postort"]);
-                    //nymedlem.epost = (string)(dr["epost"]);
-                    //nymedlem.behorighet = (int)(dr["fk_behorighet"]);
-                    
-                    //memberList.Add(nymedlem);
+                    medlem nymedlem = new medlem();
+                    nymedlem.fnamn = (string)(dr["fnamn"]);
+                    nymedlem.enamn = (string)(dr["enamn"]);
+                    nymedlem.personnr = (string)(dr["personnr"]);
+                    nymedlem.telefonnr = (string)(dr["telefonnr"]);
+                    nymedlem.adress = (string)(dr["adress"]);
+                    nymedlem.postnr = (string)(dr["postnr"]);
+                    nymedlem.postort = (string)(dr["postort"]);
+                    nymedlem.epost = (string)(dr["epost"]);
+                    nymedlem.behorighet = (int)(dr["fk_behorighet"]);
+                    memberList.Add(nymedlem);
                 }
             }
             catch (Exception ex)
@@ -152,47 +148,9 @@ namespace Korsbarsgarden
                 conn.Close();
             }
             return memberList;
-        }
-
-        public static medlem getMember(int id)
-        {
-            NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["korsbarsgarden"].ConnectionString);
-            medlem nymedlem = new medlem();
-            try
-            {
-                conn.Open();
-                string sql;
-                sql = "select * from medlem WHERE id = '"+id+"'";
-
-                NpgsqlCommand command = new NpgsqlCommand(sql, conn);
-                NpgsqlDataReader dr = command.ExecuteReader();
-                while (dr.Read())
-                {
-                    nymedlem.id = (int)(dr["id"]);
-                    nymedlem.fnamn = (string)(dr["fnamn"]);
-                    nymedlem.enamn = (string)(dr["enamn"]);
-                    nymedlem.personnr = (string)(dr["personnr"]);
-                    nymedlem.telefonnr = (string)(dr["telefonnr"]);
-                    nymedlem.adress = (string)(dr["adress"]);
-                    nymedlem.postnr = (string)(dr["postnr"]);
-                    nymedlem.postort = (string)(dr["postort"]);
-                    nymedlem.epost = (string)(dr["epost"]);
-                    nymedlem.losenord = (string)(dr["losenord"]);
-                    nymedlem.behorighet = (int)(dr["fk_behorighet"]);
-
-                }
             }
-            finally
-            {
-                conn.Close();
-            }
-            return nymedlem;
-        }
+
+        
         #endregion metoder
-
-        protected void medlemlist_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            fyllmedlem(getMember(Convert.ToInt16(medlemlist.SelectedValue)));
-        }
     }
 }
