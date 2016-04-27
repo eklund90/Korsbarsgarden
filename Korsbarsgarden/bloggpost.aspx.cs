9ecf0077c0e2d0c259b7ac980e9c90249e1f11f0
jisa1400 <jisa1400@student.miun.se>
2016-04-27 12:12:33 +0200

    Bloggkommentarer

--- Korsbarsgarden/bloggpost.aspx.cs
+++ Korsbarsgarden/bloggpost.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Npgsql;
 using System.Configuration;
+using System.Data;
 
 namespace Korsbarsgarden
 {
@@ -14,16 +15,24 @@ namespace Korsbarsgarden
         protected void Page_Load(object sender, EventArgs e)
         {            
             nyhet nyhet = new nyhet();
-            nyhet = nyhetspost(Request.QueryString["field1"]);
+            nyhet = nyhetspost(Convert.ToInt16(Request.QueryString["field1"]));
 
-            lbl_head.Text = nyhet.rubrik;
-            lbl_date.Text = "<i class='fa fa-clock-o'></i> " + nyhet.datum.ToShortDateString();
+            lbl_head.Text = nyhet.rubrik + " Av " + nyhet.skrivenav;
+            //lbl_date.Text = "<i class='fa fa-clock-o'></i> " + nyhet.datum.ToShortDateString();
             lbl_blogtext.Text = nyhet.text;
+            paragraph.InnerText = nyhet.rubrik;
+            PanelResponse_bloggpost.Visible = false;
+
+            if (!IsPostBack)
+            {
+                Repeater_kommentar.DataSource = hämtaKommentarer(Convert.ToInt16(Request.QueryString["field1"]));
+                Repeater_kommentar.DataBind();
+            }
         }
 
-        private nyhet nyhetspost(string vilkenblogg)
+        private nyhet nyhetspost(int id)
         {
-            string sql = "SELECT * FROM nyheter where rubrik = '" + vilkenblogg + "'";
+            string sql = "SELECT * FROM nyheter where id = '" + id + "'";
             nyhet aktuellnyhet = new nyhet();
             
             NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["korsbarsgarden"].ConnectionString);
@@ -40,6 +49,51 @@ namespace Korsbarsgarden
             }
             conn.Close();
             return aktuellnyhet;
+        }
+
+        private void sparaKommentar()
+        {
+            string sql = "INSERT INTO nyhetskommentar (datum, text, publicerare, fk_nyhet) VALUES (@datum, @text, @publicerare, @fknyhet)";
+
+            NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["korsbarsgarden"].ConnectionString);
+            conn.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+
+            cmd.Parameters.AddWithValue("@datum", DateTime.Now);
+            cmd.Parameters.AddWithValue("@text", txtBox_kommentar.Text);
+            cmd.Parameters.AddWithValue("@publicerare", Session["fnamn"] + " " + Session["enamn"]);
+            cmd.Parameters.AddWithValue("@fknyhet", Request.QueryString["field1"]);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            PanelResponse_bloggpost.Visible = true;
+            PanelResponse_bloggpost.CssClass = "alert-success alert PanelResponse";
+            LabelResponse_bloggpost.Text = "Kommentaren är sparad!";
+
+            txtBox_kommentar.Text = "";
+
+        }
+
+        private DataTable hämtaKommentarer(int fk)
+        {
+            string sql = "SELECT * FROM nyhetskommentar WHERE fk_nyhet = '" + fk + "'";
+            NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["korsbarsgarden"].ConnectionString);
+            conn.Open();
+            DataTable dt = new DataTable();
+
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+
+            da.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+
+        protected void btn_sparakommentar_Click(object sender, EventArgs e)
+        {
+            sparaKommentar();
+            Repeater_kommentar.DataSource = hämtaKommentarer(Convert.ToInt16(Request.QueryString["field1"]));
+            Repeater_kommentar.DataBind();
         }
     }
 }
