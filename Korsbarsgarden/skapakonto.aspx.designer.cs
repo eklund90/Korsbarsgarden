7fd5867cd03a96ba14d3f2fb6ceba8f93de1358e
eklund90 <johaneklund90@gmail.com>
2016-05-11 14:26:02 +0200

    fix

--- Korsbarsgarden/skapakonto.aspx.designer.cs
+++ Korsbarsgarden/skapakonto.aspx.designer.cs
@@ -130,6 +130,15 @@ namespace Korsbarsgarden {
         protected global::System.Web.UI.WebControls.Button btn_uppdaterakonto;
         
         /// <summary>
+        /// btn_tabortkonto control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_tabortkonto;
+        
+        /// <summary>
         /// PanelResponse_skapakonto control.
         /// </summary>
         /// <remarks>
@@ -155,14 +164,5 @@ namespace Korsbarsgarden {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListBox medlemlist;
-        
-        /// <summary>
-        /// btn_tabortkonto control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btn_tabortkonto;
     }
 }
